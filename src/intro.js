aa2d5644d37c2d66b9d5bead3d9d8aaf2e591f88
brackebusch <kyle.brackebusch@gmail.com>
2018-12-07 22:03:00 -0800

    fix more grammer errors

--- src/intro.js
+++ src/intro.js
@@ -16,7 +16,7 @@ export default class Intro extends Component {
     <div className="space" />
     <div className='inner flex-column'>
         <p>
-          Full stack developer here, I enjoy everything from creating performance oriented React components to designing database schemas with PostgreSQL for microservices. I build this website myself, with no external libraries of frameworks (except React), no Wordpress plugin here. I love learning! Challenges that demand serious thinking and invite trying new ideas are a YES in my book. I don’t shy away from big problems, and I do what it takes to get the job done well. I yearn for quality and perfection in my work. I will stay up through the night to something the right way rather than just "getting it done". My driving philosophy when coding is to write for the next developer and continually make business minded decisions.
+          Full stack developer here, I enjoy everything from creating performance oriented React components to designing database schemas with PostgreSQL for microservices. I built this website myself, with no external libraries of frameworks (except React), no Wordpress plugin here. I love learning! Challenges that demand serious thinking and invite trying new ideas are a YES in my book. I don’t shy away from big problems, and I do what it takes to get the job done well. I yearn for quality and perfection in my work. I will stay up through the night to do something the right way rather than just "get it done". My driving philosophy when coding is to write for the next developer and continually make business minded decisions.
         </p>
         <button onClick={() => this.setState({show: !this.state.show})}>{this.state.show ? 'Tell Me Less' : 'Tell Me More'}</button>
     </div>
